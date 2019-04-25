c7eb43168cea77facd2452a5ae2366b3d27e2c3a
tuyen.ta <tuyend86@gmail.com>
2019-04-25 16:17:58 +0200

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# Blockchain_PoW
+# Demos: Blockchain with Proof of Work
+Demonstration of the blockchain with Proof of Work (PoW) algorithm.
--- blockchain.py
+++ blockchain.py
@@ -43,8 +43,7 @@ def mine():
 
 @app.route('/transactions/new', methods=['POST'])
 def new_transaction():
-    values = request.get_json()
-
+    values = request.get_json(force=True)
     # Check that the required fields are in the POST'ed data
     required = ['sender', 'recipient', 'amount']
     if not all(k in values for k in required):
